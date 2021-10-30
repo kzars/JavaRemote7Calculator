b4b7dae6455ffcbdf2e2ad1646e389c51b6ee7d8
Kārlis Zars <zarskarlis@gmail.com>
2021-10-30 12:26:15 +0300

    Main structure

--- src/carpet/Carpet.java
+++ src/carpet/Carpet.java
@@ -5,8 +5,9 @@ public class Carpet {
     private double cost;
 
     public Carpet(double cost){
-        if(cost < 0) this.cost = 0;
-        else this.cost = cost;
+/*        if(cost < 0) this.cost = 0;
+        else this.cost = cost;*/
+        this.cost = (cost < 0) ? 0 : cost;
     }
 
     public double getCost() {
--- src/carpet/Floor.java
+++ src/carpet/Floor.java
@@ -6,11 +6,8 @@ public class Floor {
     private double length;
 
     public Floor(double width, double length){
-        if(width < 0) this.width = 0;
-        else this.width = width;
-
-        if(length < 0) this.length = 0;
-        else this.length = length;
+        this.length = (length < 0) ? 0 : length;
+        this.width = (width >= 0) ? width : 0;
     }
 
     public double getArea(){
