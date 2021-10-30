3ad20db0ffd14cf1a0de8c1cbea12afcce52926f
Kārlis Zars <zarskarlis@gmail.com>
2021-10-30 11:49:57 +0300

    Calculator part

--- src/carpet/Calculator.java
+++ src/carpet/Calculator.java
@@ -1,4 +1,17 @@
 package carpet;
 
 public class Calculator {
+
+    private Floor floor;
+    private Carpet carpet;
+
+    public Calculator(Floor floor, Carpet carpet){
+        this.carpet = carpet;
+        this.floor = floor;
+    }
+
+    public double getTotalCost(){
+        return (floor.getArea() * carpet.getCost());
+    }
+
 }
